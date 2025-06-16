361203b08b5579fa2dc7aef8d5bf22f32b4e2e9c
omarfn98 <161155301+omarfn98@users.noreply.github.com>
2025-06-16 21:49:52 +0200

    Create SumResponse.java

---
+++ src/main/java/com/example/calculator/model/SumResponse.java
@@ -0,0 +1,17 @@
+package com.example.calculator.model;
+
+public class SumResponse {
+    private double sum;
+
+    public SumResponse(double sum) {
+        this.sum = sum;
+    }
+
+    public double getSum() {
+        return sum;
+    }
+
+    public void setSum(double sum) {
+        this.sum = sum;
+    }
+}
